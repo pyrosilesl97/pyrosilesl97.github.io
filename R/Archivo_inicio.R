8177fcfae30b14a21394408f6830cd2cbd66349d
Pablo Yamild Rosiles Loeza <pyrosilesl97@gmail.com>
2021-08-13 16:54:22 -0500

    Problems in education data

--- R/Archivo_inicio.R
+++ R/Archivo_inicio.R
@@ -5,8 +5,10 @@ library(gitcreds)
 library(usethis)
 library(fontawesome)
 
-gert::git_commit_all('Problems in education data')
-gert::git_push()
 
 #Para procesar el resultado en html
 rmarkdown::render("index.Rmd")
+
+#To push data if the files are commited
+gert::git_commit_all('Problems in education data')
+gert::git_push()
