b0702259d79afca83898a8788d57ab8dad038813
Pablo Yamild Rosiles Loeza <pyrosilesl97@gmail.com>
2021-08-27 00:50:43 -0500

    Hola

--- R/Archivo_inicio.R
+++ R/Archivo_inicio.R
@@ -1,14 +1,3 @@
-#Para inicializar Github
-library(gert)
-library(gh)
-library(gitcreds)
-library(usethis)
-library(fontawesome)
 
-
-#Para procesar el resultado en html
+# Para poder compilar el html
 rmarkdown::render("index.Rmd")
-
-#To push data if the files are commited
-gert::git_commit_all('Cambio de foto')
-gert::git_push()
