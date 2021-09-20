5be0f0106a26a388ebddc543212c5b7333a54635
Pablo Yamild Rosiles Loeza <pyrosilesl97@gmail.com>
2021-09-20 12:18:44 -0500

    Changes

--- R/Archivo_inicio.R
+++ R/Archivo_inicio.R
@@ -1,3 +1,11 @@
 
 # Para poder compilar el html
 rmarkdown::render("index.Rmd")
+
+
+#Crear token
+usethis::create_github_token()
+#Cuando haya caducado la contraseña
+gitcreds::gitcreds_set()
+
+usethis::use_github()
